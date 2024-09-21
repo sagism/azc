cd31fb9fbd1a0d012c52bfa08496627eda3102d3
Sagi Smolarski <sagism@gmail.com>
2024-09-21 14:25:44 +0300

    new version: 1.0.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ readme_content_without_images = remove_images_from_markdown(readme_content)
 
 setup(
     name='azc',
-    version='1.0.7',
+    version='1.0.8',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
     author='Sagi Smolarski',
